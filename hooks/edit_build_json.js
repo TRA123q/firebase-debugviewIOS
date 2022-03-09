6e37fe498c31d14b0a5cb8dca43918373edf150d
TRA123q <101274679+TRA123q@users.noreply.github.com>
2022-03-09 16:37:48 -0300

    Create edit_build_json.js

---
+++ hooks/edit_build_json.js
@@ -0,0 +1,43 @@
+var fs = require('fs');
+
+if ( fs.existsSync("build.json") ) {
+
+var text = fs.readFileSync('build.json','utf8');
+var obj = JSON.parse( text );
+  
+console.log(text);
+
+if ( !obj.hasOwnProperty('ios') ) {
+obj['ios'] = {};
+}
+var obj_ios = obj['ios'];
+
+if ( !obj_ios.hasOwnProperty('debug') ) {
+obj_ios['debug'] = {};
+}
+if ( !obj_ios.hasOwnProperty('release') ) {
+obj_ios['release'] = {};
+}
+var obj_ios_debug = obj_ios['debug'];
+var obj_ios_release = obj_ios['release'];
+
+if ( !obj_ios_debug.hasOwnProperty('buildFlag') ) {
+obj_ios_debug['buildFlag'] = [];
+}
+if ( !obj_ios_release.hasOwnProperty('buildFlag') ) {
+obj_ios_release['buildFlag'] = [];
+}
+
+if( !obj_ios_debug.buildFlag.includes('-FIRDebugEnabled') ) {
+obj_ios_debug.buildFlag.push( '-FIRDebugEnabled' );
+}
+if( !obj_ios_release.buildFlag.includes('-FIRDebugEnabled') ) {
+obj_ios_release.buildFlag.push( '-FIRDebugEnabled' );
+}
+
+var json = JSON.stringify( obj, null, 2 );
+  
+console.log(json);
+  
+fs.writeFileSync('build.json', json, 'utf8');
+}
